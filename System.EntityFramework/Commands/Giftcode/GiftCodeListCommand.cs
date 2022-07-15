1dced01d90eef349f4886f851c4cb30d89fc60fc
Dominic Nguyen <khangnv111@gmail.com>
2022-07-15 22:30:57 +0700

    update

--- System.EntityFramework/Commands/Giftcode/GiftCodeListCommand.cs
+++ System.EntityFramework/Commands/Giftcode/GiftCodeListCommand.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.EntityFramework.Responsitories.BillingGiftCode;
 using System.Linq;
 using System.Models.ViewModel;
 using System.Text;
@@ -10,9 +11,10 @@ namespace System.EntityFramework.Commands.Giftcode
 {
     public class GiftCodeListCommand
     {
-
-        public GiftCodeListCommand()
+        private IGiftCodeReponsitory giftCode;
+        public GiftCodeListCommand(IGiftCodeReponsitory giftCode)
         {
+            this.giftCode = giftCode;
         }
 
         public async Task<IActionResult> ExecuteAsync(CancellationToken cancellationToken)
